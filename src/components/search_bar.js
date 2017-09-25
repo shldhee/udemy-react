61839811626c38d452e26b7a05e977e86a45836a
LEEDUCKHEE <beatdance@naver.com>
2017-09-26 01:04:24 +0900

    section1-15 class component

---
+++ src/components/search_bar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// const Component = React.Compoent;
+
+
+// const Searchbar = () => {
+// 	return <input />;
+// }; 함수형 컴포넌트
+//
+
+class SearchBar extends Component { // 문법적 설탕
+	render() {
+		return <input />;
+	}
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+// API key
+const API_KEY = 'AIzaSyAUfkH5Pc2AGhiguXKEEKVIv51sy7bip_M';
 
 // Create a new component. This component should produce
 // some HTML
 
-const App = function() {
-	return <div>Hi!</div>;
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	)
 }
 
 // Taks this component's generated HTML and put it
 // on the page (in the DOM)
 
-//<App /> 인스턴스로 만들기
-ReactDOM.render(<App />);
+//<App /> 인스턴스로 만들기 2번째 요소에 삽입
+ReactDOM.render(<App />, document.querySelector('.container'));
