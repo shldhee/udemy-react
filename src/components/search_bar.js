ac41495d4c6eea4a39c769f1a3d7ceeb8d7d1ac2
LEEDUCKHEE <beatdance@naver.com>
2017-09-27 00:56:50 +0900

    section 23

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,8 +8,20 @@ import React, { Component } from 'react';
 //
 
 class SearchBar extends Component { // 문법적 설탕
+	constructor(props) {
+		super(props);
+
+		this.state = { term: 'Starting Value' };
+	}
+
 	render() {
-		return <input />;
+		return (
+			<div>
+				<input
+					value={this.state.term}
+					onChange={event => this.setState({ term: event.target.value })} />
+			</div>
+		);
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-// API key
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAUfkH5Pc2AGhiguXKEEKVIv51sy7bip_M';
 
-// Create a new component. This component should produce
-// some HTML
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	)
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
-// Taks this component's generated HTML and put it
-// on the page (in the DOM)
-
-//<App /> 인스턴스로 만들기 2번째 요소에 삽입
 ReactDOM.render(<App />, document.querySelector('.container'));
